63e97b8663afd0fdf450a5b4b83ea2b3db928cfd
Igor Igeto Mitkovski <igetom@gmail.com>
2017-05-25 13:19:14 +0200

    25.05.2017

--- js/gameContent.js
+++ js/gameContent.js
@@ -4,6 +4,7 @@ $(() => {
     $gameContent.on("click", "#makeFirstApp", () => {
         //===================================== PRV PREGLED ==========================================
         $gameContent.append(`
+        <div class="fragment">
             <div class="row">
                 <div class="col">
                     <div><h1>Прв Преглед (5-6 недела на бременост)</h1></div>
@@ -30,6 +31,7 @@ $(() => {
                     <button id="abortBtn" class="btn btn-primary appointmentBtn">Абортирај</button>
                 </div>
             </div>
+        </div>
         `);
         $("#makeFirstApp").hide();
     });
@@ -37,39 +39,44 @@ $(() => {
 
     $gameContent.on("click", "#continueBtn", () => {
         $gameContent.append(`
-        <div class="row addBorder">
-            <div class="col">
-                <div><h5>Можни опции доколку одлучите да продолжите со бременоста:</h5></div>
+            <div class="fragment">
+                <div class="row addBorder">
+                    <div class="col">
+                        <div><h5>Можни опции доколку одлучите да продолжите со бременоста:</h5></div>
+                    </div>
+                </div>
+                <div class="row addBorder">
+                        <div class="col-6">
+                            <button id="nextAppBtn" class="btn btn-primary appointmentBtn">Закажи го следниот преглед</button>
+                        </div>
+                        <div class="col-6">
+                            <button id="jumpAppBtn" class="btn btn-primary appointmentBtn">Скокни го следниот преглед</button>
+                        </div>
+                </div>
             </div>
-        </div>
-         <div class="row addBorder">
-                <div class="col-6">
-                    <button id="nextAppBtn" class="btn btn-primary appointmentBtn">Закажи го следниот преглед</button>
-                </div>
-                <div class="col-6">
-                    <button id="jumpAppBtn" class="btn btn-primary appointmentBtn">Скокни го следниот преглед</button>
-                </div>
-         </div>`);
+        `);
          $("#continueBtn").hide();
          $("#abortBtn").hide();
     });
 
     $gameContent.on("click", "#jumpAppBtn", () => {
         $gameContent.append(`
-            <div class="row addBorder">
-                <div class="cow">
-                    <div>
-                        <h3>Не откривте дека вашиот плод има аномалија!</h3>
-                        <p><h6>Предупредување:</h6>Кај жените со тенденција за пропуштање или доцнење со одредени прегледи се зголемува опасноста од спонтан абортус или појавување на одредени проблеми во понтамошниот развој на фетусот.</p>
+            <div class="fragment">
+                <div class="row addBorder">
+                    <div class="cow">
+                        <div>
+                            <h3>Не откривте дека вашиот плод има аномалија!</h3>
+                            <p><h6>Предупредување:</h6>Кај жените со тенденција за пропуштање или доцнење со одредени прегледи се зголемува опасноста од спонтан абортус или појавување на одредени проблеми во понтамошниот развој на фетусот.</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-6">
-                    <button id="nextAppBtnAnommaly" class="btn btn-primary appointmentBtn">Закажи следен преглед</button>
-                </div>
-                <div class="col-6">
-                    <button id="jumpAllApp" class="btn btn-primary appointmentBtn">Скокни ги сите прегледи</button>
+                <div class="row">
+                    <div class="col-6">
+                        <button id="nextAppBtnAnommaly" class="btn btn-primary appointmentBtn">Закажи следен преглед</button>
+                    </div>
+                    <div class="col-6">
+                        <button id="jumpAllApp" class="btn btn-primary appointmentBtn">Скокни ги сите прегледи</button>
+                    </div>
                 </div>
             </div>
         `);
